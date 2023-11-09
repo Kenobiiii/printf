52bd616f0c73e782355b103069300f4b476996fd
paromero <paromeropons@hotmail.com>
2023-11-09 12:37:06 +0100

    cambios pequeños

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,21 @@
 /*   By: paromero <paromero@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:35:24 by paromero          #+#    #+#             */
-/*   Updated: 2023/11/08 10:36:52 by paromero         ###   ########.fr       */
+/*   Updated: 2023/11/09 12:36:46 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-int	ft_printf(char const *, ...)
-{}
+int	ft_printf(char const *format, ...)
+{
+	int	i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (format[i])
+	{
+	
+	}
+}
