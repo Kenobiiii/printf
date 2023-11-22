ec691ffd6fdf6e90841c0a6eca6a51d9eaea91c7
paromero <paromeropons@hotmail.com>
2023-11-22 12:23:52 +0100

    añadida ultima func
    añadida puthex para el %x, %X y %p

--- ft_format.c
+++ ft_format.c
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:40:37 by paromero          #+#    #+#             */
-/*   Updated: 2023/11/20 11:35:45 by paromero         ###   ########.fr       */
+/*   Updated: 2023/11/22 11:35:18 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ int	ft_putstr(char *str)
 	return (i);
 }
 
-void	ft_putnbr(int n)
+int	ft_putnbr(int n)
 {
 	int	i;
 
@@ -45,14 +45,40 @@ void	ft_putnbr(int n)
 		i += ft_putchar('0');
 	else if (n < 0)
 	{
-		ft_putchar('-');
+		i += ft_putchar('-');
 		ft_putnbr(-n);
 	}
 	else if (n > 9)
+		i += ft_putnbr(n / 10);
+	i += ft_putchar((n % 10) + '0');
+	return (i);
+}
+
+int	ft_puthex(uintptr_t n, int is_uppercase, int include_prefix)
+{
+	int		local_count;
+	char	hex_digit;
+
+	local_count = 0;
+	if (n == 0)
 	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
+		if (include_prefix)
+			local_count += ft_putstr("0x");
+		return (local_count + ft_putchar('0'));
+	}
+	if (include_prefix)
+		local_count += ft_putstr("0x");
+	if (n >= 16)
+		local_count += ft_puthex(n / 16, is_uppercase, 0);
+	hex_digit = n % 16;
+	if (hex_digit > 9)
+	{
+		if (is_uppercase)
+			local_count += ft_putchar('A' + hex_digit - 10);
+		else
+			local_count += ft_putchar('a' + hex_digit - 10);
 	}
 	else
-		ft_putchar(n + '0');
+		local_count += ft_putchar('0' + hex_digit);
+	return (local_count);
 }
