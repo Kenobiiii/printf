22a2bf78438604c9598960242ed2560dd28784af
paromero <paromeropons@hotmail.com>
2023-11-22 12:24:16 +0100

    añadido puthex

--- ft_printf.c
+++ ft_printf.c
@@ -6,50 +6,50 @@
 /*   By: paromero <paromero@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 10:35:24 by paromero          #+#    #+#             */
-/*   Updated: 2023/11/20 11:04:07 by paromero         ###   ########.fr       */
+/*   Updated: 2023/11/22 12:00:58 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-char	ft_format(char	format, va_list	arg)
+int	ft_format(char format, va_list arg)
 {
 	int	i;
 
 	i = 0;
-	if (format == "c")
+	if (format == 'c')
 		i += ft_putchar(va_arg(arg, int));
-	else if (format == "s")
-		i += ft_putstr(va_arg(arg, char	*));
-	else if (format == "p")
-		i +=;
-	else if (format == "d")
+	else if (format == 's')
+		i += ft_putstr(va_arg(arg, char *));
+	else if (format == 'p')
+		i += ft_puthex((uintptr_t)va_arg(arg, int), 0, 1);
+	else if (format == 'd')
 		i += ft_putnbr(va_arg(arg, int));
-	else if (format == "i")
-		i +=;
-	else if (format == "u")
-		i +=;
-	else if (format == "x")
-		i +=;
-	else if (format == "X")
-		i +=;
-	else if (format == "%")
-		write (1, "%", 1);
+	else if (format == 'i')
+		i += ft_putnbr(va_arg(arg, int));
+	else if (format == 'u')
+		i += ft_putnbr((unsigned int)va_arg(arg, unsigned int));
+	else if (format == 'x')
+		i += ft_puthex((uintptr_t)va_arg(arg, int), 0, 0);
+	else if (format == 'X')
+		i += ft_puthex((uintptr_t)va_arg(arg, int), 1, 0);
+	else if (format == '%')
+		i += ft_putchar('%');
 	return (i);
 }
 
 int	ft_printf(char const *format, ...)
 {
 	va_list	arg;
-	int	i;
-	int	final;
+	int		i;
+	int		final;
 
 	va_start (arg, format);
 	i = 0;
 	final = 0;
 	while (*format)
 	{
-	 	if (format[i] == '%')
+		if (format[i] == '%')
 		{
 			i++;
 			final += ft_format(format[i], arg);
