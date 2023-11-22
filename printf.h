403e2df73a59910041c994b7f2025f59b46bfe1a
paromero <paromeropons@hotmail.com>
2023-11-22 12:24:30 +0100

    añadidas resto de funciones a header

--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: paromero <paromero@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:00:24 by paromero          #+#    #+#             */
-/*   Updated: 2023/11/16 11:46:47 by paromero         ###   ########.fr       */
+/*   Updated: 2023/11/22 12:00:19 by paromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@
 # include <stdio.h>
 # include <string.h>
 # include <stdlib.h>
+# include <stdint.h>
+# include <stdarg.h>
 
 int		ft_putchar(char c);
 int		ft_putstr(char *str);
-void	ft_putnbr(int n);
+int		ft_putnbr(int n);
+int		ft_puthex(uintptr_t n, int is_uppercase, int include_prefix);
+int		ft_format(char format, va_list arg);
+int		ft_printf(char	const	*format, ...);
 
 #endif
